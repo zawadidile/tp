1eb83c4c2a12d754b5869a4441cc34a93dc33a4b
zawadidile <deog62286@gmail.com>
2026-02-27 16:48:46 +0200

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<clipboard/ClipboardPlugin.h>)
+#import <clipboard/ClipboardPlugin.h>
+#else
+@import clipboard;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ClipboardPlugin registerWithRegistrar:[registry registrarForPlugin:@"ClipboardPlugin"]];
+}
+
+@end

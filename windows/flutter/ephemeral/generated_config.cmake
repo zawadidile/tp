fab895f62a99a20f64410851ae5967cc062d9fca
zawadidile <deog62286@gmail.com>
2026-02-27 16:51:55 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\pc\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\pc\\Desktop\\advicely-app-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\pc\\flutter"
+  "PROJECT_DIR=C:\\Users\\pc\\Desktop\\advicely-app-main"
+  "FLUTTER_ROOT=C:\\Users\\pc\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\pc\\Desktop\\advicely-app-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\pc\\Desktop\\advicely-app-main"
+  "FLUTTER_TARGET=C:\\Users\\pc\\Desktop\\advicely-app-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguOQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NjczMjNkZTI4NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTg3YzE4Zjg3Mw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC44"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\pc\\Desktop\\advicely-app-main\\.dart_tool\\package_config.json"
+)
